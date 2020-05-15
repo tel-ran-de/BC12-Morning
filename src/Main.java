1c2803f632cb8170700b9aa6fb2c697192222dc5
andron13 <andron13@gmail.com>
2020-05-15 11:53:34 +0200

    lesson 17 - binar

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-import lesson16.*;
-import lesson17.BinarToDecimal;
-import lesson17.MyFirstSuperGame;
+import lesson17.ConvertDigits;
 
 public class Main {
 	public static void main(String[] args) {
 		//MyFirstSuperGame.startGame();
-		BinarToDecimal.convertDigits();
+		ConvertDigits.decimalToBinar();
 	}
 }
--- src/lesson17/ConvertDigits.java
+++ src/lesson17/ConvertDigits.java
@@ -2,9 +2,9 @@ package lesson17;
 
 import java.util.Scanner;
 
-public class BinarToDecimal {
+public class ConvertDigits {
 
-	public static void convertDigits() {
+	public static void decimalToBinar() {
 
 		Scanner input = new Scanner(System.in);
 		int decimal, remainder;
@@ -20,5 +20,20 @@ public class BinarToDecimal {
 			decimal = decimal / 2;
 		}
 		System.out.println(output);
+	}
+
+	// двоичные в десятичные
+	public static void binaryToDecimal() {
+		Scanner input = new Scanner(System.in);
+		int remainder, binary; // remainder - остаток
+		String output = "";
+		System.out.println("Insert your number");
+		binary = input.nextInt();
+	}
+
+	public static void testMethode() {
+		String test = "0757";
+		int decimalTest = Integer.parseInt(test, 8);
+		System.out.println(decimalTest);
 	}
 }
