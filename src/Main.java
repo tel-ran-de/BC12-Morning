4f2e9032f4de674ad64837e795874fd9a8aaa51c
andron13 <andron13@gmail.com>
2020-05-15 10:20:41 +0200

    lesson 17 - game

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,8 @@
 import lesson16.*;
+import lesson17.MyFirstSuperGame;
 
 public class Main {
 	public static void main(String[] args) {
-		// write your code here
-		// размеры комнаты и стоимость плитки
-		//Tile.customerInfo(7, 4, 5);
-
-		// Результаты выборов
-		// MisterPresident.electionResult();
-		System.out.println(AlcoholAndChild.kasirka());
-
+		MyFirstSuperGame.startGame();
 	}
 }
