87fd56506fbe1433628970e107b169dd5e48ae95
andron13 <andron13@gmail.com>
2020-05-27 12:18:06 +0200

    Lesson24 arrays introduction

---
+++ src/lesson24/ArrayExample.java
@@ -0,0 +1,9 @@
+package lesson24;
+
+/**
+ * Created for BC12-Morning.
+ * User: andron13
+ * Date: 27.05.2020; Time: 11:29
+ */
+public class ArrayExample {
+}
---
+++ src/lesson24/ArraysExercises.java
@@ -0,0 +1,9 @@
+package lesson24;
+
+/**
+ * Created for BC12-Morning.
+ * User: andron13
+ * Date: 27.05.2020; Time: 10:42
+ */
+public class ArraysExercises {
+}
