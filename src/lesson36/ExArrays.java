d6e30761c99334b738f7618a43c6e6fe4e59166c
andron13 <andron13@gmail.com>
2020-06-12 12:40:46 +0200

    lesson36

--- src/lesson36/ExArrays.java
+++ src/lesson36/ExArrays.java
@@ -1,6 +1,5 @@
 package lesson36;
 
-
 import java.util.Arrays;
 
 /**
@@ -9,9 +8,22 @@ import java.util.Arrays;
  */
 public class ExArrays {
     public static void main(String[] args) {
-        //слияние двух массивов
         int[] a1 = {25, 14, 2, 18, 36};
         int[] a2 = {1, 2, 3, 4, 5};
+        mergeTest(a1,a2);
+        // System.out.println(Arrays.toString(anotherMerge(a1, a2)));
+    }
+
+/*    private static int[] anotherMerge(int[] a1, int[] a2) {
+        int[] result;
+       // result = ArrayUtils.addAll(intArray, intArray2);
+
+        return result;
+    }*/
+
+    private static void mergeTest(int[] a1, int[] a2) {
+        //слияние двух массивов
+
         int[] c = new int[a1.length + a2.length];
 
         for (int i = 0; i < a1.length; i++) {
