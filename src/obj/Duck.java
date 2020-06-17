f52e128eff8e27894906ba4103c243cdd683a33a
andron13 <andron13@gmail.com>
2020-06-17 09:23:54 +0200

    object day two

---
+++ src/obj/Duck.java
@@ -0,0 +1,31 @@
+package obj;
+
+/**
+ * BC12-Morning
+ * 16 12 : 36
+ */
+public class Duck {
+    String breed;
+    int age;
+    String color;
+
+    // утка плавать, крякать, летать, ест
+
+
+    void swims(){
+        System.out.println("Duck swims!");
+    }
+
+    void quacks(){
+        System.out.println("Duck quacks!");
+    }
+
+    void eats(){
+        System.out.println("Duck eats");
+    }
+
+    void flies(){
+        System.out.println("I CAN FLY!!!");
+        System.out.println("I FLY!!!");
+    }
+}
---
+++ src/obj/StartTres.java
@@ -0,0 +1,16 @@
+package obj;
+
+/**
+ * BC12-Morning
+ * 16 12 : 35
+ */
+public class StartTres {
+    public static void main(String[] args) {
+        Duck greyDuck = new Duck();
+        greyDuck.age = 4;
+        greyDuck.breed = "anasPlatyrhynchos";
+        greyDuck.color = "grey";
+        greyDuck.flies();
+    }
+}
+
